7f9efe381e80559daa03f3f6962437d3517a5759
Dax Harris <mvh3875@rit.edu>
2025-02-18 19:50:53 -0500

    wip derive

--- crates/ormox_derive/src/document.rs
+++ crates/ormox_derive/src/document.rs
@@ -1,1 +1,15 @@
+use darling::{FromDeriveInput, FromMeta};
+use proc_macro::TokenStream;
+
+#[derive(FromDeriveInput, Default)]
+#[darling(attributes(ormox), forward_attrs(allow, doc, cfg))]
+pub struct DocumentOpts {
+    ident: syn::Ident,
+    attrs: Vec<syn::Attribute>,
+    collection: String
+}
+
+pub(crate) fn derive_doc(input: TokenStream) -> TokenStream {
+    
+}
 
